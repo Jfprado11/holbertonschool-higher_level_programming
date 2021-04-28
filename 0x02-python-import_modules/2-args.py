37271f5b4d13aa29b98e0bdb4d7d79022ab89f8f
Felipe Prado <jfpc11@misena.edu.co>
2021-04-28 17:27:48 -0500

    implimente in import from and just getting module argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def main():
-    import sys
-    lenArgv = len(sys.argv)
+    from sys import argv
+    lenArgv = len(argv)
     if lenArgv == 2:
         print("{:d} argument:".format(lenArgv - 1))
     elif lenArgv <= 1:
@@ -9,7 +9,7 @@ def main():
     else:
         print("{:d} arguments:".format(lenArgv - 1))
     for i in range(1, lenArgv):
-        print("{:d} : {:s}".format(i, sys.argv[i]))
+        print("{:d} : {:s}".format(i, argv[i]))
 
 if __name__ == "__main__":
     main()
