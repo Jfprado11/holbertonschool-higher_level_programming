d3fd4ab4fe2158700952a5b1fc98f33bc2ed796b
Felipe Prado <jfpc11@misena.edu.co>
2021-04-28 18:35:47 -0500

    use dir function and print its content

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def main():
+    import hidden_4
+    hid = dir(hidden_4)
+    for i in len(hid):
+        if hid[i][0] != "_":
+            print(hid)
+
+if __name__ == "__main__":
+    main()
