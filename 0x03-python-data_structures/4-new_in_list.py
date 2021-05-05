43678a757760ec09285dc60cefeefcbfe8d68700
Felipe Prado <jfpc11@misena.edu.co>
2021-05-05 17:09:20 -0500

    a copy of a list a change some values

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    new_list = my_list.copy()
+    len_list = len(new_list)
+    if idx < 0:
+        return new_list
+    elif idx + 1 > len_list:
+        return new_list
+    else:
+        new_list[idx] = element
+        return new_list
