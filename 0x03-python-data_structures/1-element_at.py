e4585bfe655f4205c912aed890682b03ac19eee2
Felipe Prado <jfpc11@misena.edu.co>
2021-05-05 09:33:04 -0500

    a function to search for specific element in a list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/user/bin/python3
+def element_at(my_list, idx):
+    lenList = len(my_list)
+    if idx < 0:
+        return None
+    elif idx > lenList:
+        return None
+    else:
+        return my_list[idx]
