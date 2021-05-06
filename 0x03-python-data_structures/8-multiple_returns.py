a0b2ef39753217fa5793cddea4ca51fe6349f34f
Felipe Prado <jfpc11@misena.edu.co>
2021-05-05 21:21:30 -0500

    adding if the string is empty

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == "":
-        return None
     len_str = len(sentence)
-    first_char = sentence[0]
+    if len(sentence) == 0:
+        first_char = None
+    else:
+        first_char = sentence[0]
     tup = len_str, first_char
     return tup
