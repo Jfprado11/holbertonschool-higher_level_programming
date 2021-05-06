346ca051e9c0cd52d505f69d3e80da7ec6022062
Felipe Prado <jfpc11@misena.edu.co>
2021-05-06 18:36:33 -0500

    printing a sorted diccionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for hey, val in sorted(a_dictionary.items()):
-        print(hey, ":", val)
+    for hey in sorted(a_dictionary.keys()):
+        print(hey, ":", a_dictionary[hey])
+
