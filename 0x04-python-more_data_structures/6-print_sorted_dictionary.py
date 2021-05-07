cdd03026cc896b4da2067b0ee5e54edb19963c91
Felipe Prado <jfpc11@misena.edu.co>
2021-05-06 20:11:09 -0500

    without keys

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    new_lists = sorted(list(a_dictionary))
-    for hey in new_lists:
-        print(hey, ":", a_dictionary[hey])
+    for hey in sorted(a_dictionary.keys()):
+        print("{} : {}".format(hey, a_dictionary[hey]))
