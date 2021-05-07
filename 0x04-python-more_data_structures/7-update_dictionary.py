5b384245ee7eeda0577ec67360c40a971ee59422
Felipe Prado <jfpc11@misena.edu.co>
2021-05-06 20:43:35 -0500

    two lines

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    for hey in list(a_dictionary):
-        if hey == key:
-            a_dictionary[hey] = value
-        a_dictionary[key] = value
+    a_dictionary[key] = value
     return a_dictionary
