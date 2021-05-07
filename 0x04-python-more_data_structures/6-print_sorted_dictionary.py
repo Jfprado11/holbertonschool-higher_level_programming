bb1e1165419153043e9fe734b113835fbf918a89
Felipe Prado <jfpc11@misena.edu.co>
2021-05-06 20:08:41 -0500

    sorted diccionary but first is converted to a lists';

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for hey in sorted(a_dictionary.keys()):
+    new_lists = list(sorted(a_dictionary.keys()))
+    for hey in new_lists:
         print(hey, ":", a_dictionary[hey])
-
