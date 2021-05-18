f3278b91dbf13eaba7c4a6d0008cb9a50f468856
Felipe Prado <jfpc11@misena.edu.co>
2021-05-17 19:58:15 -0500

    safe interger in stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,5 +5,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except:
-        sys.stderr.write("Exception:\n")
+        print("Exception:", file=sys.stderr)
         return False
