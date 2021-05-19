e8207e527fb3375e4fdd5e0552589d5f974c6a61
Felipe Prado <jfpc11@misena.edu.co>
2021-05-18 20:43:22 -0500

    initializing the class with a size value

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""learning how to use class and methods
+for this excersize we will create a class
+squere to know the squre root of some functions"""
+
+
+class Square:
+    """defines a square by"""
+
+    def __init__(self, size):
+        """ init the class"""
+        self.__size = size
