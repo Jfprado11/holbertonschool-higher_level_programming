da88426f9412960b15a664543dbb06dece28707b
Felipe Prado <jfpc11@misena.edu.co>
2021-05-18 21:02:28 -0500

    adding to the class some statements to checks is atributte size is a int

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""learning how to use class and methods
+for this excersize we will create a class
+squere to know the squre root of some functions"""
+
+
+class Square:
+    """defines a square by"""
+    def __init__(self, size=0):
+        """init the class
+        args:
+            param 1(size): the size of squere"""
+        if isinstance(size, int) is True:
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
