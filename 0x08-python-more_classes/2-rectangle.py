b8b392cabd5bee4aae917d4fe71b17397d84c585
Felipe Prado <jfpc11@misena.edu.co>
2021-05-20 19:03:49 -0500

    trying the louvani way

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -10,6 +10,16 @@ class Rectangle:
         self.width = width
         self.height = height
 
+    def area(self):
+        """method to get the area of a rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """method the get the area of a rectangle"""
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return (self.__width * 2) + (self.__height * 2)
+
     @property
     def width(self):
         """retriving width as a private instance"""
@@ -43,13 +53,3 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if self.__height < 0:
             raise ValueError("height must be >= 0")
-
-    def area(self):
-        """method to get the area of a rectangle"""
-        return self.__width * self.__height
-
-    def perimeter(self):
-        """method the get the area of a rectangle"""
-        if self.__height == 0 or self.__width == 0:
-            return 0
-        return (self.__width * 2) + (self.__height * 2)
