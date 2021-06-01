ef7c5302693ac1bfaf51d355a01b6c592e7d1f95
Felipe Prado <jfpc11@misena.edu.co>
2021-06-01 18:35:20 -0500

    adding the init function

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,6 +7,10 @@ that inherentce from the object reacriting a list
 class MyList(list):
     """A class that inherits from a list"""
 
+    def __init__(self):
+        """initialization of the class"""
+        super().__init__()
+
     def print_sorted(self):
         """prints a sorted array"""
         print(sorted(self))
