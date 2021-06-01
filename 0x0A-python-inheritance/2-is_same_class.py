12161d860b8909a5e7e060f399e117230c8df113
Felipe Prado <jfpc11@misena.edu.co>
2021-06-01 17:15:40 -0500

    is a instance of specific object

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""a function that check if an
+object is an instance
+"""
+
+
+def is_same_class(obj, a_class):
+    """a function that check if a
+    object is a is instance of a class"""
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
