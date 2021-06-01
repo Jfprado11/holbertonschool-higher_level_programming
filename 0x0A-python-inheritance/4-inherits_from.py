094b5e69126cf48fb0e1f05d0a7f866e79a93a60
Felipe Prado <jfpc11@misena.edu.co>
2021-06-01 18:27:44 -0500

    adding if is the same class do not return true

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,6 +6,8 @@ inherited (directly or indirectly) from the specified class
 
 
 def inherits_from(obj, a_class):
+    if type(obj) is a_class:
+        return False
     if issubclass(type(obj), a_class):
         return True
     else:
