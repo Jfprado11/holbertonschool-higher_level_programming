25e7b8d8071ca14b039262204f737d539814cae1
Felipe Prado <jfpc11@misena.edu.co>
2021-06-01 18:50:05 -0500

    raises an error

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A module witch it hold a
+class base geometry
+"""
+
+
+class BaseGeometry:
+    """A class with a method
+    that raises an Error
+    """
+
+    def area(self):
+        """ A method that
+        raises an error
+        """
+        raise Exception("area() is not implemented")
