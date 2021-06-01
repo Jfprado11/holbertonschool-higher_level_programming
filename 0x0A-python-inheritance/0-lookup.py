675f7dc65059ec66275d5b09844333e12ce2e79b
Felipe Prado <jfpc11@misena.edu.co>
2021-06-01 13:46:21 -0500

    adding comments:

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""a module taht has a function
+witch return the methods of an objecta can hold"""
+
+
 def lookup(obj):
+    """a function that recives an object
+    and return a list with its methods that the object can
+    hold"""
     return dir(obj)
