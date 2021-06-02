f368bb65eafb4c000855980ba7138e92f1fd3610
Felipe Prado <jfpc11@misena.edu.co>
2021-06-01 20:47:39 -0500

    passing the rectangle class to square class

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""A method that holds a class
+witch it inherates from the class
+rectangle
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """a class witch holds
+    an inheratence of rectangle
+    """
+
+    def __init__(self, size):
+        """ initializate the class
+        with its atributes
+        """
+        super().__init__(size, size)
+        self.__size = size
+        self.integer_validator("size", size)
+
+    def area(self):
+        """gives the area of a square"""
+        return self.__size * self.__size
