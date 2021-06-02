e487eaac6b4783b294613cbf7e98418bc2f13eb9
Felipe Prado <jfpc11@misena.edu.co>
2021-06-01 21:54:28 -0500

    square str overrainding

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""A method that holds a class
+witch it inherates from the class
+rectangle
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """a class witch holds
+    an inheratence of rectangle
+    """
+
+    def __init__(self, size):
+        """ initiation of the class
+        containing the super class to bring up the methods
+        from the paretn class
+        """
+        super().__init__(size, size)
+        super().__str__()
+        self.__size = size
+        self.integer_validator("size", self.__size)
+
+    def area(self):
+        """gives the area of a square"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
