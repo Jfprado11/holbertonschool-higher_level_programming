ffee96480741fce9036bdf139f2aad6c87c6dbdb
Felipe Prado <jfpc11@misena.edu.co>
2021-06-08 21:07:11 -0500

    adding some cases

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -140,6 +140,15 @@ class TestRectangleClass(unittest.TestCase):
         r1.update(x=1, height=2, y=3, width=4)
         self.assertEqual(r1.__str__(), "[Rectangle] (89) 1/3 - 4/2")
 
+    def test_dict_rectangle(self):
+        """checking if the dictionary prints correctly"""
+        r1 = Rectangle(10, 2, 1, 9)
+        self.assertEqual(r1.to_dictionary(), {
+                         'x': 1, 'y': 9, 'id': 1, 'height': 2, 'width': 10})
+        r2 = Rectangle(8, 9)
+        self.assertEqual(r2.to_dictionary(), {
+                         'x': 0, 'y': 0, 'id': 2, 'height': 9, 'width': 8})
+
 
 if __name__ == '__main__':
     unittest.main()
