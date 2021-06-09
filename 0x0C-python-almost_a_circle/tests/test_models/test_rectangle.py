900f55d24b7a190f5149cad0444f87608c980500
Felipe Prado <jfpc11@misena.edu.co>
2021-06-08 20:41:01 -0500

    adding some test to rectangle

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -16,6 +16,7 @@ class TestRectangleClass(unittest.TestCase):
     """
 
     def setUp(self):
+        """the setup of the classes of the test cases"""
         Base._Base__nb_objects = 0
 
     def test_id_rectangle(self):
@@ -124,6 +125,7 @@ class TestRectangleClass(unittest.TestCase):
         self.assertEqual(r1.__str__(), "[Rectangle] (89) 4/5 - 2/3")
 
     def test_update_1(self):
+        """checking update 1 with some cases"""
         r1 = Rectangle(10, 10, 10, 10)
 
         r1.update(height=1)
@@ -137,6 +139,15 @@ class TestRectangleClass(unittest.TestCase):
 
         r1.update(x=1, height=2, y=3, width=4)
         self.assertEqual(r1.__str__(), "[Rectangle] (89) 1/3 - 4/2")
+
+    def test_dict_rectangle(self):
+        """checking if the dictionary prints correctly"""
+        r1 = Rectangle(10, 2, 1, 9)
+        self.assertEqual(r1.to_dictionary(), {
+                         'x': 1, 'y': 9, 'id': 1, 'height': 2, 'width': 10})
+        r2 = Rectangle(8, 9)
+        self.assertEqual(r2.to_dictionary(), {
+                         'x': 0, 'y': 0, 'id': 2, 'height': 9, 'width': 8})
 
 
 if __name__ == '__main__':
