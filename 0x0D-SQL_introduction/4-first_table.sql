f37e14d41dc163a588299b41463fedb58e824742
Felipe Prado <jfpc11@misena.edu.co>
2021-07-12 15:26:22 -0500

    adding a databases

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 -- creating a table
-CREATE TABLE IF TABLE NOT EXISTS first_table (
-       id INT,
-       name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS first_table (
+id INT,
+name VARCHAR(256)
 );
