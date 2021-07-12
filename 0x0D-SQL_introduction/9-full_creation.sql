9544de37fbe37b6f4a791aa27a2243969efe6374
Felipe Prado <jfpc11@misena.edu.co>
2021-07-12 16:19:09 -0500

    adding more entetties

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+-- creates a second table
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+INSERT INTO second_table (id, name, score) VALUES(1, "John", 10);
+INSERT INTO second_table (id, name, score) VALUES(2, "John", 3);
+INSERT INTO second_table (id, name, score) VALUES(3, "John", 14);
+INSERT INTO second_table (id, name, score) VALUES(4, "John", 8);
