41c1afcb580aac2a6e40bedc63d8d4a9a1efbcc3
Felipe Prado <jfpc11@misena.edu.co>
2021-07-13 21:52:48 -0500

    fixing the join function

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,5 +2,5 @@
 -- that are containing in hbtn_0d_usa.
 SELECT cities.id, cities.name, states.name
 FROM cities, states
-WHERE cities.id = states.id
+WHERE cities.state_id = states.id
 ORDER BY cities.id ASC;
