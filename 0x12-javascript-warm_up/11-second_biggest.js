b91fc59ea3d384758962fc9ab18062b9e21f8b6f
Felipe Prado <jfpc11@misena.edu.co>
2021-08-02 18:12:41 -0500

    jmmmmm

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,4 +3,12 @@
 const args = process.argv;
 if (args.length === 2 || args.length === 3) {
   console.log(0);
+} else {
+  const arr = [];
+  for (let i = 0; i < args.length; i++) {
+    if (!isNaN(parseInt(args[i]))) {
+      arr.push(args[i]);
+    }
+  }
+  console.log(arr);
 }
