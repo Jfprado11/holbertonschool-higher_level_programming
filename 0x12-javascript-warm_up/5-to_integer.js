d4218d2e1cdc522e2062a42e8e2cf4c03c77ca99
Felipe Prado <jfpc11@misena.edu.co>
2021-08-02 13:32:18 -0500

    adding a integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,8 +3,8 @@ const args = process.argv;
 
 const num = parseInt(args[2]);
 
-if (num === NaN || !args[2]) {
-  console.log('Not a number')
+if (isNaN(num) || !args[2]) {
+  console.log('Not a number');
 } else {
   console.log('my number:', num);
 }
