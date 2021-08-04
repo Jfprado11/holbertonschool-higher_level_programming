60dd7a8e9ebd1f14a5d5ea0f2e9f6f0f60b70a05
Felipe Prado <jfpc11@misena.edu.co>
2021-08-04 13:38:37 -0500

    adding two methods

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      let arr = "X";
+      for (let j = 1; j < this.width; j++) {
+        arr = arr + "X";
+      }
+      console.log(arr);
+    }
+  }
+
+  rotate() {
+    let x = this.width;
+    this.width = this.height;
+    this.height = x;
+  }
+
+  double() {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
