6807a46fb5c525605e08e95e80e51c71fa575cd4
Felipe Prado <jfpc11@misena.edu.co>
2021-08-04 13:39:52 -0500

    fixing semi

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,29 +1,29 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
 
-  print() {
+  print () {
     for (let i = 0; i < this.height; i++) {
-      let arr = "X";
+      let arr = 'X';
       for (let j = 1; j < this.width; j++) {
-        arr = arr + "X";
+        arr = arr + 'X';
       }
       console.log(arr);
     }
   }
 
-  rotate() {
-    let x = this.width;
+  rotate () {
+    const x = this.width;
     this.width = this.height;
     this.height = x;
   }
 
-  double() {
+  double () {
     this.width = this.width * 2;
     this.height = this.height * 2;
   }
