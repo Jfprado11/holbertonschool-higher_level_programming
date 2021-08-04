f16ff4155fc7e36219517ef3eb5c39b28784473c
Felipe Prado <jfpc11@misena.edu.co>
2021-08-04 11:54:13 -0500

    addi a mehtod to print a rectangle

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -10,7 +10,7 @@ module.exports = class Rectangle {
   print () {
     for (let i = 0; i < this.height; i++) {
       let arr = 'X';
-      for (let j = 0; j < this.width; j++) {
+      for (let j = 1; j < this.width; j++) {
         arr = arr + 'X';
       }
       console.log(arr);
