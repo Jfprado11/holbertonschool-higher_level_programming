e647220f765ffc24c5d25614558ba4d181108722
Felipe Prado <jfpc11@misena.edu.co>
2021-08-04 19:03:21 -0500

    reversing an array

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 exports.esrever = function (list) {
   const arr = [];
   arr.splice(0, arr.length);
-  for (let x = list.length - 1; x > 0; x--) {
+  for (let x = list.length - 1; x >= 0; x--) {
     arr.push(list[x]);
   }
   return arr;
