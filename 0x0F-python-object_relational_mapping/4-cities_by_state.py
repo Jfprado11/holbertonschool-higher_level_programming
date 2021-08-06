05ca6258e8d5014da35609b7ea387674afbb858d
Felipe Prado <jfpc11@misena.edu.co>
2021-08-06 10:20:44 -0500

    using a join in sql

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+lists all states from the database hbtn_0e_0_usa
+"""
+if __name__ == "__main__":
+
+    import MySQLdb
+    import sys
+
+    root = sys.argv[1]
+    passw = sys.argv[2]
+    data = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost", user=root, passwd=passw,
+                         db=data, port=3306)
+
+    cursor = db.cursor()
+
+    sql = """SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states
+    ON cities.state_id = states.id
+    ORDER BY 'cities.id' ASC"""
+
+    cursor.execute(sql)
+
+    result = cursor.fetchall()
+
+    for x in result:
+        print(x)
+
+    cursor.close()
+    db.close()
