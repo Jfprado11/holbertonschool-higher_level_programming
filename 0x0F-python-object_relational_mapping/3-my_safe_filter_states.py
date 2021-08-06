2688a7333679cbb9c6bd1d91379af745754fba0f
Felipe Prado <jfpc11@misena.edu.co>
2021-08-06 09:36:08 -0500

    without format to safe sql

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument.
+"""
+if __name__ == "__main__":
+
+    import MySQLdb
+    import sys
+
+    root = sys.argv[1]
+    passw = sys.argv[2]
+    data = sys.argv[3]
+    name_search = sys.argv[4]
+
+    db = MySQLdb.connect(host="localhost", user=root, passwd=passw,
+                         db=data, port=3306)
+
+    cursor = db.cursor()
+
+    sql = """SELECT * FROM states
+    WHERE name LIKE %s ORDER BY 'states.id' ASC;"""
+
+    cursor.execute(sql, (name_search,))
+
+    result = cursor.fetchall()
+
+    for x in result:
+        print(x)
+
+    cursor.close()
+    db.close()
