1d296572783d51ba1008a1d407bdfeb039139561
Felipe Prado <jfpc11@misena.edu.co>
2021-08-20 12:07:31 -0500

    omg please

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,6 +5,5 @@ import requests
 
 if __name__ == "__main__":
     r = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print("    - type: {}".format(type(r.text)))
-    print("    - content: {}".format(r.text))
+    print("Body response:\n    - type: {}\n    - content: {}".format(
+        type(r.text), r.text))
