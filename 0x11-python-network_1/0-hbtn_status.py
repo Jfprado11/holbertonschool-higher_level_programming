e0793b833b7e5affe46a73ce2236fd6cc81d2212
Felipe Prado <jfpc11@misena.edu.co>
2021-08-20 09:31:16 -0500

    detching a web page

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""usign urlib to fetch some pages"""
+
+import urllib.request
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        data = response.read()
+        print("    - type: {}".format(type(data)))
+        print("    - content: {}".format(data))
+        print("    - utf8 content: {}".format(data.decode("UTF-8")))
