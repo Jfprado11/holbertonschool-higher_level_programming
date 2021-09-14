099984d6c8f81f704bc92ec24205af7b8431cf05
Felipe Prado <jfpc11@misena.edu.co>
2021-09-14 16:46:53 -0500

    reading a file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const fs = require("fs");
+
+const args = process.argv;
+
+const file = args[2];
+
+fs.readFile(file, "utf8", (err, data) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(data);
+});
