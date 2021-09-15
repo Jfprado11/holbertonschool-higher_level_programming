ef21105c4655f9e9393a1cee1ab5e1e44d374886
Felipe Prado <jfpc11@misena.edu.co>
2021-09-14 19:59:01 -0500

    adding if theres is not user

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,14 +2,14 @@
 
 const url = process.argv[2];
 
-const request = require('request');
+const request = require("request");
 request.get({ url: url, json: true }, function (error, r, body) {
   if (error) {
     console.error(error);
   }
   const result = {};
   for (const user of body) {
-    if (!(user.userId in result)) {
+    if (!(user.userId in result) && user.completed) {
       result[user.userId] = 0;
     }
     if (user.completed === true) {
