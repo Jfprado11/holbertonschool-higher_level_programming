e46f460f6a0b449964bd78d9a1f8b0ef59316479
Felipe Prado <jfpc11@misena.edu.co>
2021-09-14 19:54:11 -0500

    retriving the exact output

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const url = process.argv[2];
+
+const request = require('request');
+request.get({ url: url, json: true }, function (error, r, body) {
+  if (error) {
+    console.error(error);
+  }
+  const result = {};
+  for (const user of body) {
+    if (!(user.userId in result)) {
+      result[user.userId] = 0;
+    }
+    if (user.completed === true) {
+      result[user.userId] += 1;
+    }
+  }
+  console.log(result);
+});
